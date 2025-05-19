67eac7b932069d22c17a552897fd920e0f411160
RickBally <ballyrichard02@gmail.com>
2025-05-19 12:11:18 +0200

    Finished login UI

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -4,25 +4,25 @@ import './../css/login.css';
 
 export function Login() {
   return (
-    <div className="Page">
-      <div>Bejelentkezés</div>
-      <div>
+    <div className="page">
+    <div className="container">
+      <div className='gridItems'>
+        <div id="login">Bejelentkezés</div>
         <div>
-          <label>Felhasználó név</label>
-          <input type='text' required></input>
+          <input id='textInput' type='text' required placeholder='Felhasználónév'></input>
         </div>
         <div>
-          <label>Jelszó</label>
-          <input type='password' required></input>
+          <input id='textInput' type='password' required placeholder='Jelszó'></input>
         </div>
         <div>
-          <input type='button' value={'Bejelentkezés'}></input>
+          <input id='buttonInput' type='button' value={'Bejelentkezés'}></input>
         </div>
         <div>
           <label>Nincs fiókod? </label>
           <Link to="/register">Regisztrálj</Link>
         </div>
       </div>
+    </div>
     </div>
   );
 }
