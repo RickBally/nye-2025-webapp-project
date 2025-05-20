8a6ff678aee1816d1fa6a0486030b88b15feb538
RickBally <ballyrichard02@gmail.com>
2025-05-20 09:45:16 +0200

    Updated Page content

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import './../css/home.css';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 export function Home() {
+    const { user, logout } = useAuth();
+    
     return (
-        <div>Home</div>
+        <div>
+            <div className='navBar'>
+                {user ? (
+                    <>
+                        <Link id='navigateButton' to="/account"><div id='navigateText'>Fiókom</div></Link>
+                        <input id='navigateButton' type='button' onClick={logout} value={'Kijelentkezés'}></input>
+                    </>
+                ) : (
+                    <>
+                        <Link id='navigateButton' to="/login"><div id='navigateText'>Bejelentkezés</div></Link>
+                        <Link id='navigateButton' to="/register"><div id='navigateText'>Regisztráicó</div></Link>
+                    </>
+                )}
+            </div>
+            <div className='page'>
+                <div className='homeBody'>
+                    {user ? (
+                        <div>Üdvözlünk újra, {user.name}!</div>
+                    ) : (
+                        <div>Üdvözlünk! Kérem, jelentkezzen be!</div>
+                    )}
+                </div>
+            </div>
+        </div>
     );
 }
